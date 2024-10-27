2bbea6f03289e8027430006cff692cec800f4090
Ken Yeh <yehken@gmail.com>
2024-10-26 19:49:55 -0700

    set up folders and files

---
+++ data/dogs.js
@@ -0,0 +1,16 @@
+const dogs = [
+  {
+    "id": 1,
+    "name": "Sparky",
+    "breed": "Chihuahua",
+    "sex": "male",
+    "age": 16
+  },
+  {
+    "id": 2,
+    "name": "Fido",
+    "breed": "Poodle",
+    "sex": "male",
+    "age": 5
+  },
+]
--- index.js
+++ index.js
@@ -7,5 +7,5 @@ PORT = 3000;
 
 
 app.listen(3000, () => {
-  console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
